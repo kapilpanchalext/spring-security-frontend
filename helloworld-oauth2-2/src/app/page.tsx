7dfe2f7f5b449803e58149308449ed602d7376e4
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2024-12-05 11:48:49 +0530

    [debug]

--- helloworld-oauth2-2/src/app/page.tsx
+++ helloworld-oauth2-2/src/app/page.tsx
@@ -1,9 +1,16 @@
-import styles from "./page.module.scss";
+"use client"
+// import styles from "./page.module.scss";
 
 export default function Home() {
+
+  const buttonClickHandler = () => {
+    alert("HelloWorld");
+  }
+
   return (
     <>
       <h1>HelloWorld</h1>
+      <button onClick={buttonClickHandler}>Click</button>
     </>
   );
 }
