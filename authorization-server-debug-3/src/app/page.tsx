79e5a30011974354167fa258d6d775d85c075d37
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-01-16 08:15:07 +0530

    [refactor]

--- authorization-server-debug-3/src/app/page.tsx
+++ authorization-server-debug-3/src/app/page.tsx
@@ -101,46 +101,112 @@ export default function Home() {
   const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Send Request To Auth Server Handler");
+    const loginData = new URLSearchParams();
+  
+    // Replace with the actual input field names from the form
+    loginData.append('username', 'admin@email.com');
+    loginData.append('password', '1234');
 
-  const baseURL = 'http://localhost:8084/oauth2/authorize';
-  const queryParams = {
-    response_type: 'code',
-    client_id: 'capstone-project-auth-code-pkce-1',
-    redirect_uri: 'http://localhost:3000',
-    scope: 'openid email',
-    state: '4qFl3tTCkYb2R6pD',
-    code_challenge: 'Tgc1QidrfeRMUExvgLljq621HlAIkc5YJ7NmUfGiryA',
-    code_challenge_method: 'S256',
-  };
-
-  const url = new URL(baseURL);
-  Object.entries(queryParams).forEach(([key, value]) => {
-    url.searchParams.append(key, value);
-  });
-
-  console.log("Constructed URL:", url.toString());
-
-  try {
-    const response = await fetch(url.toString(), {
-      method: 'GET',
-        //   headers: {
-        //     "Content-Type": "application/x-www-form-urlencoded",
-        //     Authorization: "Basic " + btoa("admin@email.com:1234"),
-        // },
-        redirect: 'manual',
+    const baseURL = 'http://localhost:8084/oauth2/authorize';
+    const queryParams = {
+      response_type: 'code',
+      client_id: 'capstone-project-auth-code-pkce-1',
+      redirect_uri: 'http://localhost:3000',
+      scope: 'openid email',
+      state: '4qFl3tTCkYb2R6pD',
+      code_challenge: 'Tgc1QidrfeRMUExvgLljq621HlAIkc5YJ7NmUfGiryA',
+      code_challenge_method: 'S256',
+    };
+    
+    const url = new URL(baseURL);
+    Object.entries(queryParams).forEach(([key, value]) => {
+      url.searchParams.append(key, value);
     });
 
-    if (response.ok) {
-      const redirectUrl = response.url;
-      // console.log("Redirect URL: ", redirectUrl);
-      console.log('Headers:', JSON.stringify(response.headers));
-      // console.dir("Response: ", JSON.stringify(response));
-    } else {
-      throw new Error(`Error: ${response.statusText}`);
+    console.log("Constructed URL:", url.toString());
+
+    try {
+      const response = await fetch(url.toString(), {
+        method: 'GET',
+        credentials: "include",
+        //     headers: {
+        //       "Content-Type": "application/x-www-form-urlencoded",
+        //       Authorization: "Basic " + btoa("admin@email.com:1234"),
+        //   },
+        // redirect: 'manual',
+        // body: loginData.toString(),
+      });
+
+      if (response.ok) {
+        const redirectUrl = response.url;
+        const cookies = document.cookie;
+        console.log(cookies);
+
+        if (!cookies) {
+          throw new Error("No cookies found in the response.");
+        }
+
+        // Parse cookies to find the XSRF token
+        const xsrfTokenMatch = cookies.match(/XSRF-TOKEN=([^;]+)/);
+        if (!xsrfTokenMatch) {
+          throw new Error("Failed to extract XSRF token from cookies.");
+        }
+        const csrfToken = xsrfTokenMatch[1];
+        console.log("Extracted XSRF Token:", csrfToken);
+    
+        if (!csrfToken) {
+          throw new Error("Failed to extract CSRF token from login page.");
+        }
+        loginData.append("_csrf", csrfToken);
+        // console.log("Redirect URL: ", redirectUrl);
+        // console.log('Headers:', JSON.stringify(response.headers));
+        // console.dir("Response: ", JSON.stringify(response));
+        console.log("URL: ", redirectUrl);
+        console.log("Status:", response.status);
+        console.log("Headers:");
+
+        response.headers.forEach((value, name) => {
+          console.log(`${name}: ${value}`);
+        });
+
+        const htmlContent = await response.text();
+        console.log("HTML Content:", htmlContent);
+
+
+        const urlRedirected = new URL(redirectUrl);
+        // Object.entries(queryParams).forEach(([key, value]) => {
+        //   urlRedirected.searchParams.append(key, value);
+        // });
+
+        console.log("Login Data: ", loginData.get('username'));
+
+        const responsePost = await fetch(urlRedirected, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: "Basic " + btoa("admin@email.com:1234"),
+            // "X-XSRF-TOKEN": csrfToken,
+          },
+          body: loginData.toString(),
+          credentials: "include",
+          // redirect: 'manual',
+        });
+
+        if (responsePost.ok) {
+          console.log("1 Login successful!");
+          console.log("1 Redirecting to:", responsePost.headers.get('Location'));
+        } else if (responsePost.status === 302 || response.status === 301) {
+          console.log("1 Redirect to:", responsePost.headers.get('Location'));
+          // Optionally, follow the redirect manually
+        } else {
+          console.error("1 Login failed:", responsePost.status, responsePost.statusText);
+        }
+      } else {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+    } catch (err) {
+      console.error("Error in authorization request:", err.message);
     }
-  } catch (err) {
-    console.error("Error in authorization request:", err.message);
-  }
   };
   
   return (
