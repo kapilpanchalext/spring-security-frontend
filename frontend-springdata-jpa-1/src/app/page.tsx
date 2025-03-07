3251f8e65705c403dbd5b80b1b81023d11fcdceb
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-03-07 14:25:50 +0530

    [refactor] Add a new field to the page.tsx file

--- frontend-springdata-jpa-1/src/app/page.tsx
+++ frontend-springdata-jpa-1/src/app/page.tsx
@@ -1,9 +1,33 @@
+"use client"
+import { use, useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const[data, setData] = useState([]);
+  async function fetchStudentCourseSubjects() {
+    try {
+      const response = await fetch("http://localhost:9001/api/v1/courses-subjects");
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+      // alert(JSON.stringify(data));
+      return data;
+    } catch (error) {
+      console.error("Error fetching student course subjects:", error);
+    }
+  }
+
+  // Call the function
+  useEffect(() => {
+    fetchStudentCourseSubjects();
+  }, []);
+
   return (
     <>
-      <h1>HelloWorld</h1>
+      <h1>{JSON.stringify(data)}</h1>
     </>
   );
 };
